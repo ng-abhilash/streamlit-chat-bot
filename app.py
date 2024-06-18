2e696184c227be55f8cfdd40376a562d9cf3279c
ng-abhilash <abhilash.saini@ngenux.com>
2024-06-18 10:40:29 +0530

    updated with css

--- app.py
+++ app.py
@@ -11,52 +11,6 @@ WEBSOCKET_URL = st.secrets["WEBSOCKET_URL"]
 
 st.title("Chat Bot")
 
-
-async def chat_with_websocket(message):
-    async with websockets.connect(WEBSOCKET_URL) as websocket:
-        await websocket.send(json.dumps({"action": "message", "message": message}))
-        
-        bot_response = "" 
-
-        while True:
-            try:
-                response = await websocket.recv()
-                response_data = json.loads(response)
-                res = response_data['message']
-                if res == "Internal server error":
-                    break
-                bot_response+=res
-                st.session_state.chat_history[-1] = f'<div class="bot"><p>{bot_response}</p></div>'
-                response_placeholder.markdown("\n".join(st.session_state.chat_history), unsafe_allow_html=True)
-            except websockets.exceptions.ConnectionClosed:
-                if bot_response == "":
-                    bot_response = "Sorry, I'm unable load the data please try again"
-                    st.session_state.chat_history[-1] = f'<div class="bot"><p>{bot_response}</p></div>'
-
-def periodic_call():
-    asyncio.run(chat_with_websocket("user_input__"))
-    threading.Timer(14.30 * 60, periodic_call).start()
-
-if 'chat_history' not in st.session_state:
-    periodic_call()
-    st.session_state['chat_history'] = []
-
-
-response_placeholder = st.empty()
-
-
-
-
-
-user_input = st.chat_input("Type your message here:", key=f"user_input")
-
-if user_input:
-    if user_input.strip() != "":
-        st.session_state.chat_history.append(f'<div class="user"><p>{user_input}</p></div>')
-        st.session_state.chat_history.append('<div class="bot"><p>...</p></div>')
-        response_placeholder.markdown("\n".join(st.session_state.chat_history), unsafe_allow_html=True)
-        asyncio.run(chat_with_websocket(user_input))
-
 st.markdown(
     """
     <style>
@@ -87,3 +41,55 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
+
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] = ['<div class="bot"><p>Welcome to ChatBot!</p></div>']
+
+
+response_placeholder = st.empty()
+
+
+user_input = st.chat_input("Type your message here:", key=f"user_input")
+async def chat_with_websocket(message):
+    async with websockets.connect(WEBSOCKET_URL) as websocket:
+        await websocket.send(json.dumps({"action": "message", "message": message}))        
+        bot_response = "" 
+        while True:
+            try:
+                response = await websocket.recv()
+                response_data = json.loads(response)
+                res = response_data.get('message')
+                if res:
+                    if res == "Internal server error":
+                        break
+                    bot_response+=res
+                    st.session_state.chat_history[-1] = f'<div class="bot"><p>{bot_response}</p></div>'
+                    response_placeholder.markdown("\n".join(st.session_state.chat_history), unsafe_allow_html=True)
+            except websockets.exceptions.ConnectionClosed:
+                if bot_response == "":
+                    bot_response = "Sorry, I'm unable load the data please try again"
+                    st.session_state.chat_history[-1] = f'<div class="bot"><p>{bot_response}</p></div>'
+
+
+
+def periodic_call():
+    response_placeholder.markdown("\n".join(st.session_state.chat_history), unsafe_allow_html=True)
+    asyncio.run(chat_with_websocket("user_input__"))
+    
+    threading.Timer(5, periodic_call).start()
+
+if 'timer_started' not in st.session_state:
+    response_placeholder.markdown("\n".join(st.session_state.chat_history), unsafe_allow_html=True)
+    st.session_state.timer_started = True
+    periodic_call()
+
+
+
+if user_input:
+    if user_input.strip() != "":
+        st.session_state.chat_history.append(f'<div class="user"><p>{user_input}</p></div>')
+        st.session_state.chat_history.append('<div class="bot"><p>...</p></div>')
+        response_placeholder.markdown("\n".join(st.session_state.chat_history), unsafe_allow_html=True)
+        asyncio.run(chat_with_websocket(user_input))
+    else:
+        response_placeholder.markdown("\n".join(st.session_state.chat_history), unsafe_allow_html=True)
